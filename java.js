00f7329b149a9ea31d7244f32009ee5b8ecbb2fc
abdullahi_megenow <amegenow@pdx.edu>
2025-12-09 23:33:53 -0800

    Added Navbar but since everyone think is delayed by one I won't see how it look just yet

--- java.js
+++ java.js
@@ -1,1 +1,6 @@
-ajsdklaasd;
+const toggleBtn = document.getElementById("toggleBtn");
+const navLinks = document.getElementById("navLinks");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
